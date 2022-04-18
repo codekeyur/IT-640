75a1592db4cac1a4c2f007fa5f042edb1d09974a
codekeyur <keyurpatel2868@gmail.com>
2022-04-18 13:19:32 -0400

    IT-640 LAMP Stack Project

---
+++ LAMP Stack  Setup/db.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE Employee_Database.employee (
+    employee_id integer NOT NULL,
+    first_name character(45) NOT NULL,
+    last_name character(45) NOT NULL,
+    email character (45) NOT NULL,
+    phone character (45) NOT NULL,
+    Department character(45)
+);
+
+
+
+
+
+INSERT INTO Employee_Database.employee VALUES (1, 'Braelyn', 'Richmond', 'Braelyn1Richmond@slsteam.com', '(628) 737-8387', 'Accounting');
+INSERT INTO Employee_Database.employee VALUES (2, 'Kameron', 'Benson', 'Kameron2Benson@slsteam.com', '(412) 542-1964', 'Accounting');
+INSERT INTO Employee_Database.employee VALUES (3, 'Hugo', 'Randolph', 'Hugo3Randolph@slsteam.com', '(977) 281-4408', 'Sales');
+INSERT INTO Employee_Database.employee VALUES (4, 'Adalyn', 'Aguilar', 'Adalyn4Aguilar@slsteam.com', '(897) 406-2496', 'HR');
+INSERT INTO Employee_Database.employee VALUES (5, 'Deborah', 'Stevens', 'Deborah5Stevens@slsteam.com', '(295) 952-1762', 'IT');
+INSERT INTO Employee_Database.employee VALUES (6, 'Riya', 'Valdez', 'Riya6Valdez@slsteam.com', '(456) 630-2623', 'IT');
+INSERT INTO Employee_Database.employee VALUES (7, 'Paityn', 'Liu', 'Paityn7Liu@slsteam.com', '(357) 707-9281', 'Engineering');
+INSERT INTO Employee_Database.employee VALUES (8, 'Dorian', 'Gray', 'Dorian8Gray@slsteam.com', '(342) 587-7675', 'Engineering');
+INSERT INTO Employee_Database.employee VALUES (9, 'Lillie', 'Madden', 'Lillie9Madden@slsteam.com', '(242) 303-5504', 'DevOps');
+INSERT INTO Employee_Database.employee VALUES (10, 'Marlie', 'Carson', 'Marlie10Carson@slsteam.com', '(840) 215-7859', 'HR');
+INSERT INTO Employee_Database.employee VALUES (11, 'Eric', 'Little', 'Eric11Little@slsteam.com', '(240) 946-4206', 'DevOps');
+INSERT INTO Employee_Database.employee VALUES (12, 'Draven', 'Bauer', 'Draven12Bauer@slsteam.com', '(752) 320-9309', 'Sales');
+INSERT INTO Employee_Database.employee VALUES (13, 'Judith', 'Walker', 'Judith13Walker@slsteam.com', '(298) 754-7168', 'Engineering');
+INSERT INTO Employee_Database.employee VALUES (14, 'Paris', 'Villa', 'Paris14Villa@slsteam.com', '(747) 652-9428', 'DevOps');
+INSERT INTO Employee_Database.employee VALUES (15, 'Francesca', 'Valencia', 'Francesca15Valencia@slsteam.com', '(871) 849-2091', 'HR');
+
+
---
+++ LAMP Stack  Setup/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "P@ssw0rd";
+$db = "Employee DB";
+
+$conn = new mysqli($servername, $username, $password, $db);
+?>
